2e3c6eda64dd77f65ea7cdf607ff436cdb7d9d3a
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-06-06 15:39:47 -0400

    remove old code

--- array_ops.c
+++
@@ -1,8 +0,0 @@
-#include "array_ops.h"
-
-void ints_exscan(int const* a, int n, int* o)
-{
-  o[0] = 0;
-  for (int i = 0; i < n; ++i)
-    o[i + 1] = o[i] + a[i];
-}
--- array_ops.h
+++
@@ -1,6 +0,0 @@
-#ifndef ARRAY_OPS
-#define ARRAY_OPS
-
-void ints_exscan(int const* a, int n, int* o);
-
-#endif
