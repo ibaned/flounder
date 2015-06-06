827fec0742feb1cec64bc422323945033dc6f603
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-06-06 17:04:46 -0400

    "production" main code
    starting to see nice gradation, 1K elements

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 double size_fun(struct x x)
 {
   (void)x;
-  return 0.05 + (.3) * x.x[0];
+  return 1e-4 + (5e-3) * x.x[0];
 }
 
 int main()
@@ -23,21 +23,21 @@ int main()
   };
   struct rgraph fvs = rgraph_new_from_dat(2, 3, fvs_dat);
   struct xs xs = xs_new_from_dat(4, x_dat);
-  char buf[64];
-  for (int i = 0; i < 4; ++i) {
-    printf("round %d\n", i );
+  int done = 0;
+  while (!done) {
     struct ss dss = gen_size_field(fvs, xs, size_fun);
     struct rgraph fvs2;
     struct xs xs2;
     refine(fvs, xs, dss, &fvs2, &xs2);
+    if (fvs.nverts == fvs2.nverts)
+      done = 1;
     ss_free(dss);
     rgraph_free(fvs);
     xs_free(xs);
     fvs = fvs2;
     xs = xs2;
-    sprintf(buf, "out_%d.vtk", i);
-    write_vtk(buf, fvs, xs);
   }
+  write_vtk("out.vtk", fvs, xs);
   xs_free(xs);
   rgraph_free(fvs);
   return 0;
