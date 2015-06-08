15960a216bd1bb50ba0e2a0bef01dd06a01a0e2f
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-06-08 16:13:20 -0400

    going for gold with new cosine size field

--- test.c
+++ test.c
@@ -1,3 +1,6 @@
+/* makes M_PI visible */
+#define _XOPEN_SOURCE 500
+#include <math.h>
 #include "refine.h"
 #include "size.h"
 #include "vtk.h"
@@ -16,6 +19,20 @@ double ring(struct x x)
   return 5e-7 + d * 5e-5;
 }
 
+double sinusoid(struct x x)
+{
+  double s = cos(x.x[0] * 8.0 * M_PI) / 4.0 + 1.0 / 2.0;
+  double d = fabs(x.x[1] - s);
+  return 1e-6 + d * 1e-4;
+}
+
+double gold_sinusoid(struct x x)
+{
+  double s = cos(x.x[0] * 8.0 * M_PI) / 4.0 + 1.0 / 2.0;
+  double d = fabs(x.x[1] - s);
+  return 1e-7 + d * 1e-5;
+}
+
 int main()
 {
   int const fvs_dat[6] = {
@@ -32,7 +49,7 @@ int main()
   struct xs xs = xs_new_from_dat(4, x_dat);
   int done = 0;
   while (!done) {
-    struct ss dss = gen_size_field(fvs, xs, ring);
+    struct ss dss = gen_size_field(fvs, xs, gold_sinusoid);
     struct rgraph fvs2;
     struct xs xs2;
     refine(fvs, xs, dss, &fvs2, &xs2);
@@ -44,7 +61,8 @@ int main()
     fvs = fvs2;
     xs = xs2;
   }
-  write_vtk("out.vtk", fvs, xs);
+  printf("num faces %d\n", fvs.nverts);
+/*write_vtk("out.vtk", fvs, xs);*/
   xs_free(xs);
   rgraph_free(fvs);
   return 0;
